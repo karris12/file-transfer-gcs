67d10a31cf9282539d0014e40d6df2fa348a8d7e
karris12 <112893792+karris12@users.noreply.github.com>
2024-10-14 19:34:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from google.cloud import storage
+
+def transfer_file(event, context):
+    source_bucket_name = 'jethro123a'
+    destination_bucket_name = 'chris231b'
+
+    # Extract the file name and other details from the event
+    file_name = event['name']
+
+    # Initialize the storage client
+    storage_client = storage.Client()
+
+    # Get the source and destination buckets
+    source_bucket = storage_client.bucket(source_bucket_name)
+    destination_bucket = storage_client.bucket(destination_bucket_name)
+
+    # Get the blob from the source bucket
+    source_blob = source_bucket.blob(file_name)
+
+    # Copy the blob to the destination bucket
+    destination_blob = source_bucket.copy_blob(source_blob, destination_bucket, file_name)
+
+    print(f'File {file_name} transferred from {source_bucket_name} to {destination_bucket_name}')
 
